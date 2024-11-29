b8235493b69f90f8d8be3d680567c4e90378be14
BalvasAA <113370083+AlexanderWaves@users.noreply.github.com>
2024-11-29 11:01:08 +0300

    Добавлена функция вычитания

--- calc_oper.py
+++ calc_oper.py
@@ -6,3 +6,8 @@ def addition(a, b):
     a = validate_decimal(a)  # Валидация входных данных
     b = validate_decimal(b)  # Валидация входных данных
     return a + b
+
+def subtraction(a, b):
+    a = validate_decimal(a)
+    b = validate_decimal(b)
+    return a - b
